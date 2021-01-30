97b2905c8c4ce3cf8f6757edfddfe46f10bdb179
Felix Bossio <felixbossio@gmail.com>
2021-01-30 07:20:42 -0500

    Fix bug in format_datetime function

--- app.py
+++ app.py
@@ -62,12 +62,16 @@ class Artist(db.Model):
 #----------------------------------------------------------------------------#
 
 def format_datetime(value, format='medium'):
-  date = dateutil.parser.parse(value)
+  #date = dateutil.parser.parse(value)
+  if isinstance(value, str):
+        date = dateutil.parser.parse(value)
+    else:
+        date = value
   if format == 'full':
       format="EEEE MMMM, d, y 'at' h:mma"
   elif format == 'medium':
       format="EE MM, dd, y h:mma"
-  return babel.dates.format_datetime(date, format)
+  return babel.dates.format_datetime(date, format, locale='en')
 
 app.jinja_env.filters['datetime'] = format_datetime
 
